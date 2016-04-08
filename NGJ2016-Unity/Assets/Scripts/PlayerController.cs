4c83234698fcd4e2e8b2fbf755c6e8f039997749
Luke Spierewka <spierek@gmail.com>
2016-04-09 00:08:07 +0200

    [+] Shooting lasers

--- NGJ2016-Unity/Assets/Scripts/PlayerController.cs
+++ NGJ2016-Unity/Assets/Scripts/PlayerController.cs
@@ -5,23 +5,37 @@ using System.Collections.Generic;
 [RequireComponent(typeof(CircleCollider2D))]
 public class PlayerController : LSCacheBehaviour {
 	#region Variables
+	[Header("Movement")]
 	[SerializeField, Range(0,200)]
 	private float acceleration = 10f;
 	[SerializeField, Range(0,200)]
 	private float friction = 70f;
+
+	[Header("Prefabs")]
+	[SerializeField]
+	private GameObject laserPrefab;
+
+	[Header("Dirs")]
+	[SerializeField]
+	private Transform paintDir;
 
 	private const string HORIZONTAL_AXIS_NAME = "Horizontal";
 	private const string VERTICAL_AXIS_NAME =	"Vertical";
 	#endregion
 
 	#region Monobehaviour
-	private void Awake() {
-
+	private void Start() {
+		LSDebug.SetEnabled(true);
 	}
 	
 	private void Update() {
 		rigidbody2D.drag = friction;
 		Move();
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Shoot();
+		}
 	}
 	#endregion
 
@@ -35,6 +49,24 @@ public class PlayerController : LSCacheBehaviour {
 
 		// apply input
 		rigidbody2D.AddForce(movement * acceleration * Time.deltaTime, ForceMode2D.Impulse);
+	}
+
+	private void Shoot()
+	{
+		// get firing direction
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+		Vector3 forward = worldMousePos - transform.position;
+
+		// calculate rotation angle
+		float rotationAngle = -LSGamepad.GetStickAngle(forward.x, forward.y);
+
+		Debug.DrawLine(transform.position, worldMousePos, Color.red);
+
+		// spawn player laser
+		Transform t = LSUtils.InstantiateAndParent(laserPrefab, paintDir);
+		t.position = transform.position;
+		t.localRotation = Quaternion.Euler(0, 0, rotationAngle);
 	}
 	#endregion
 }
