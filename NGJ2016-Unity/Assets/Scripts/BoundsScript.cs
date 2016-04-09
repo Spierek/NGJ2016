ef0a1210cbed760fd0553a70c8b69cc3e3c7983e
Luke Spierewka <spierek@gmail.com>
2016-04-09 18:29:00 +0200

    [+] Spawn randomization

---
+++ NGJ2016-Unity/Assets/Scripts/BoundsScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BoundsScript : MonoBehaviour {
+	#region Variables
+	public static BoundsScript Instance;
+
+	public Bounds bounds;
+	#endregion
+
+	#region Monobehaviour
+	private void Awake()
+	{
+		Instance = this;
+	}
+	#endregion
+}
--- NGJ2016-Unity/Assets/Scripts/EnemySpawner.cs
+++ NGJ2016-Unity/Assets/Scripts/EnemySpawner.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 using System.Collections;
 
 public class EnemySpawner : MonoBehaviour {
 	#region Variables
 	[SerializeField]
-	private Vector2 spawnRate = new Vector2(1f, 2f);
+	private Vector2 spawnDelay = new Vector2(1f, 2f);
+	[SerializeField, Range(0,10f)]
+	private float minPlayerDistance = 1f;
+
 	[SerializeField]
 	private Transform spawnDir;
 
@@ -16,19 +18,43 @@ public class EnemySpawner : MonoBehaviour {
 
 	#region Monobehaviour
 	private void Start() {
-		StartCoroutine(Spawn());
+		StartCoroutine(WaitAndSpawn());
 	}
 	#endregion
 
 	#region Methods
-	private IEnumerator Spawn()
+	private IEnumerator WaitAndSpawn()
 	{
+		float delay = Random.Range(spawnDelay.x, spawnDelay.y);
+		yield return new WaitForSeconds(delay);
+
+		// check if spawning isn't happening to close to the player
+		do
+		{
+			RandomizePosition();
+		}
+		while (CheckPlayerDistance());
+
 		Transform t = LSUtils.InstantiateAndParent(enemyPrefab, spawnDir);
 		t.position = transform.position;
 
-		float delay = Random.Range(spawnRate.x, spawnRate.y);
-		yield return new WaitForSeconds(delay);
-		StartCoroutine(Spawn());
+		StartCoroutine(WaitAndSpawn());
+	}
+
+	private void RandomizePosition()
+	{
+		Bounds bounds = BoundsScript.Instance.bounds;
+		Vector3 newPos = new Vector3(
+			Random.Range(-bounds.extents.x, bounds.extents.x),
+            Random.Range(-bounds.extents.y, bounds.extents.y),
+			0);
+
+		transform.position = newPos;
+	}
+
+	private bool CheckPlayerDistance()
+	{
+		return minPlayerDistance >= Vector3.Distance(PlayerController.Instance.transform.position, transform.position);
 	}
 	#endregion
 }
